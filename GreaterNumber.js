c5d9f21be938a49fde690a3072937e5fe8d17a51
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2023-12-11 16:49:15 +0200

    Create GreaterNumber.js

---
+++ GreaterNumber.js
@@ -0,0 +1,16 @@
+function celichisla(input){
+
+
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    
+    if (num1 > num2){
+        console.log(num1)
+    }
+       else{
+        console.log(num2)
+    
+       } 
+    
+}
+celichisla([5,3])
