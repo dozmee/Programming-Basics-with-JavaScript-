4f8539ed28b9b72bd3119492d8734956ca3039b4
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2023-12-11 16:50:26 +0200

    Create text.js

---
+++ text.js
@@ -0,0 +1,12 @@
+function chisla(input) {
+
+    let text = input[0]
+
+    for (let i = 0; i < text.length; i++) {
+        let bukva = text[i];
+        console.log(bukva);
+
+    }
+
+}
+chisla(["vlado"])
