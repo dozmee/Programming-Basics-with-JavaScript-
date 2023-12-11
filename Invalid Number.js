9f5faa41198fa73cc8b6bc3fc1a3effda320a90e
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2023-12-11 16:49:27 +0200

    Create Invalid Number.js

---
+++ Invalid Number.js
@@ -0,0 +1,14 @@
+function daysOfweek(input) {
+
+    let chislo = Number(input[0]);
+
+    if(chislo >= 100 && chislo <=200 || chislo === 0){
+
+        console.log();
+    }else {
+        console.log("invalid");
+    }
+
+}
+daysOfweek([75
+])
