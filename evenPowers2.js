c36312cffb82987c302cb92bc2fc8efff2907841
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2023-12-22 12:58:54 +0200

    Create evenPowers2.js

---
+++ evenPowers2.js
@@ -0,0 +1,12 @@
+function chisla(input) {
+
+    let n = Number(input[0])
+
+    for (let i = 0; i <= n; i+=2){
+        let naVtora = Math.pow(2,i)
+        console.log(naVtora);
+    }
+        
+    
+}
+chisla([10])
