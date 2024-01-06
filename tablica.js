80a69f8212391991150dc292a9fa6f8ba4188907
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2024-01-06 15:22:31 +0200

    Create tablica.js

---
+++ tablica.js
@@ -0,0 +1,19 @@
+let techBrands = [
+    {
+        name:"Facebook",
+        price:341
+    },
+    {
+        name: "Apple",
+        price: 500
+    },
+    {
+        name: "Amazon",
+        price: 400
+    },
+    
+   ]
+      console.table(techBrands);
+
+        
+
